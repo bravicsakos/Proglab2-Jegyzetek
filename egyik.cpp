3e34c0b55e8eee2fbd598bcf8d58b262ebac3a33
Bravics Ákos <36420147+bravicsakos@users.noreply.github.com>
2018-12-02 10:09:18 +0100

    Add files via upload

---
+++ egyik.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+template <class T, int N> class Array{
+private:
+	T data[N];
+public:
+	Array(T def = 0);
+	T operator [] (int  index) const;
+	template <int M> Array<T, N> & operator = (Array<T, M> arr);
+
+
+
+
+};
+
+
+template <class T, int N> Array<T, N>::Array(T def){ //template szerepeljen az osztály nevében, array után a t
+	for(int i = 0; i<N; i++) {
+		data[i]=def;
+	}
+
+}
+
+template <class T, int N> T Array<T,N>::operator [](int index) const{ //azért jó ezt csinálni, mert a függvény működhet másként mint az alap definiált
+   return index >=0 && index < N ? data[index]:0.0;
+
+
+}
+
+
+template <class T, int N> template <int M> Array<T, N> & Array<T, N> :: operator = (Array<T, M> arr){
+
+	for(int i = 0; i<(N>M?M:N); i++){
+		data[i]=arr[i];
+	}
+
+
+
+	return *this;
+
+}
+
+
+int main(){
+//Array<double> a(2);
+/*	Array <double, 3> b (8.2);  //az array double és array long két külön osztály, csak a mintájuk egyezik meg
+	Array<long, 8> a (2.5);
+	Array<long, 3> c (2.5); //három különböző típusparaméternél három konstruktor
+
+
+
+
+for(int i = 0; i<10; i++){
+	std::cout << a[i] << ' ' << b[i]<< std::endl;
+}
+*/
+
+//legnehezebb kérdés: hogyan lehetne felüldefiniálni az értékadót úgy, hogy az a=c futattható legyen, és az a elemeihez a c elemeit rendeli, ha létezik olyan index mindkettőben
+	Array<long, 8> a (2);
+		Array<long, 6> c (1);
+
+
+a=c;
+
+for(int i = 0; i<10; i++){
+	std::cout << a[i] << std::endl;
+}
+
+
+
+}
---
+++ masik.cpp
@@ -0,0 +1,80 @@
+﻿#include<stdio.h>
+#include <iostream>
+
+#define TMAX(a, b) a>b? a:b //makró szövegszerű behelyettesítés, a makró előnye, hogy nem tartalmaz típusnevet
+
+inline int tmax(int a, int b){ //kód behelyettesítés függvényhívás helyett
+
+	return a>b? a: b;
+
+
+}
+
+template <class T, class U> inline T tmax(T a, U b){ //t vagy u típusnak veszi mindet, az összes típusnak meg kell jelenni a paraméterlistában
+
+	return a>b? a: b;
+
+
+}
+
+
+inline double tmax(double a, double b){ //kód behelyettesítés függvényhívás helyett
+
+	return a>b? a: b;
+
+
+}
+
+class A {};
+
+int operator > (A arg1, A arg2){
+
+	return 0;
+
+}
+
+
+int main(){
+
+	int i =5;
+	int j =8;
+	int k;
+
+	k=TMAX(i++, j++); //k = i++>j++ ? i++:j++
+    //printf("i: %d j: %d k: %d\n", i, j, k);
+
+    std::cout  << "i: " << i<<" j: "<<j<<" k: "<<k <<"\n";
+
+    i =5;
+    j =8;
+    k=tmax(i++, j++);
+
+   // printf("i: %d j: %d k: %d\n", i, j, k);
+
+    std::cout  << "i: " << i<<" j: "<<j<<" k: "<<k <<"\n";
+
+
+	double i =5.2;
+		double j =8.2;
+		double k;
+
+		k=TMAX(i++, j++);
+
+		 std::cout  << "i: " << i<<" j: "<<j<<" k: "<<k <<"\n";
+
+		 i =5.2;
+		    j =8.2;
+		    k=tmax(i++, j++);
+
+		    std::cout  << "i: " << i<<" j: "<<j<<" k: "<<k <<"\n"; */
+
+
+
+	tmax(2, 3);
+	tmax(3.4, 5.1); //akárhányszor új típussal kell legyártani, új kód keletkezik belőle az exeben
+     tmax("as","bs");
+   A a, b;
+   tmax(a,b); // kell hozzá az operátor túlterhelés, hogy menjen
+
+
+}
